bb58a047d914baf6443f664c53bc18361a4a6e42
auntjoestar <88282065+Auntjoestar@users.noreply.github.com>
2026-03-10 11:04:27 -0600

    Improve responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { FwbButton, FwbInput, } from 'flowbite-vue';
+import { FwbButton, FwbInput, FwbP, } from 'flowbite-vue';
 import AppTable from './components/AppTable.vue';
 import { reactive, ref, toRaw, type Ref } from 'vue';
 import type { Row } from './types/Row';
 import { STgcfr } from './utils/math/euclidean_algorithms';
 
-const number1: Ref<number> = ref(0);
-const number2: Ref<number> = ref(0);
+const number1: Ref<number | undefined> = ref(undefined);
+const number2: Ref<number | undefined> = ref(undefined);
 
 const rows = ref<Row[]>([])
 
@@ -16,6 +16,14 @@ const congruentPositiveNumber = ref<number | null>(null)
 const congruentPositiveNumber2 = ref<number | null>(null)
 
 const calculateSTgcfr = () => {
+    if (!number1.value || !number2.value) {
+        return
+    }
+
+    if (typeof number1.value !== "number" || typeof number2.value !== "number") {
+        return
+    }
+
     const gen = STgcfr(number1.value, number2.value)
 
     const result = [...gen]
@@ -23,7 +31,7 @@ const calculateSTgcfr = () => {
     rows.value = result
 
     if (gen.next().done) {
-        over.value = !over.value
+        over.value = true
 
         const lastItem = rows.value[rows.value.length - 1]
         if (lastItem && lastItem?.s < 0) {
@@ -35,20 +43,25 @@ const calculateSTgcfr = () => {
 </script>
 
 <template>
-    <div class="w-full h-screen py-10 px-4 md:px-30 dark:text-white dark:bg-gray-800 overflow-scroll">
+    <div class="w-full h-screen py-10 px-4 md:px-30 dark:text-white dark:bg-gray-800 overflow-scroll"
+        @keyup.enter="calculateSTgcfr">
         <h1 class="font-extrabold text-2xl text-center">Extended Euclidean Algorithm Tabular Compiler</h1>
-        <div class="flex-col justify-center mt-2">
-            <div class="flex gap-8 justify-center w-full items-center px-2 md:px-0 wrap">
-                <FwbInput v-model="number1" placeholder="128" label="Enter first number: " class="grow basis-lg" />
-                <FwbInput v-model="number2" placeholder="1428" label="Enter second number: " class="grow basis-lg" />
+        <div class="flex flex-col justify-center mt-2 md:px-60">
+            <div>
+                <FwbInput v-model="number1" type="number" placeholder="128" label="First number: "
+                    class="grow basis-lg" />
 
-
-                <FwbButton @click="calculateSTgcfr" class="h-10 mt-6">
-                    Calculate
-                </FwbButton>
             </div>
+            <div class="mt-4">
+                <FwbInput v-model="number2" type="number" placeholder="1428" label="Second number: "
+                    class="grow basis-lg" />
+
+            </div>
+            <FwbButton @click="calculateSTgcfr" class="p-2.5 md:p-3 mt-4 w-20 self-end">
+                Calculate
+            </FwbButton>
         </div>
-        <div class="mt-8">
+        <div class="mt-4">
             <AppTable :rows />
         </div>
         <div v-if="over" class="mt-4">
@@ -63,4 +76,12 @@ const calculateSTgcfr = () => {
 
 <style scoped>
 @reference "./style.css";
+
+:deep(.custom-error) {
+    @apply text-red-400 mt-2 mr-2
+}
+
+:deep(.calculate-button) {
+    @apply text-center h-10 p-2.5 self-center mt-12 sm:mt-6
+}
 </style>
